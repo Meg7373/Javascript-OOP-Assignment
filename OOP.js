649ae2061daa0c988292b3b393ae3172310c3df6
Meg7373 <meaghannlw@gmail.com>
2026-01-07 13:51:54 -0500

    update js

---
+++ OOP.js
@@ -0,0 +1,10 @@
+// Create a Payment class in JavaScript
+
+class Payment {
+    constructor (payerName, amount, method, date) {
+        this.payerName = payerName
+        this.amount = amount
+        this.method = method
+        this.date = date
+    }
+}
